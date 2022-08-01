de996f4b529236fd917e8fd3ea229b8d782376e2
Igor Anic <ianic@inet.hr>
2022-08-01 21:27:23 +0200

    cleanup pwm example
    Leave only pwm related stuff.

--- pwm/src/main.zig
+++ pwm/src/main.zig
@@ -3,75 +3,23 @@ const micro = @import("microzig");
 const chip = micro.chip;
 const regs = chip.registers;
 
-var ticks: u32 = 0;
-
 pub const interrupts = struct {
-    pub fn SysTick() void {
-        tick();
-    }
+    pub fn SysTick() void {}
 
     pub fn EXTI0() void {
-        //tim_overflows += 1;
         regs.EXTI.PR.modify(.{ .PR0 = 1 });
-        //regs.TIM9.SR.modify(.{ .TIF = 0 });
-    }
-
-    pub fn TIM1_UP_TIM10() void {
-        // Check and clear status register UIF = Update interrupt pending
-        if (regs.TIM10.SR.read().UIF == 1) {
-            regs.TIM10.SR.modify(.{ .UIF = 0 });
-            tim_overflows += 1;
-        }
     }
 };
-
-const Task = struct {
-    frame: anyframe = undefined,
-    resume_at: u32 = 0,
-
-    const Self = @This();
-
-    pub fn schedule(self: *Self, frame: anyframe, after: u32) void {
-        self.frame = frame;
-        self.resume_at = ticks + after;
-    }
-
-    pub fn runnable(self: *Self) bool {
-        return !(self.frame == undefined or
-            self.resume_at == 0 or
-            self.resume_at > ticks);
-    }
-
-    pub fn try_run(self: *Self) void {
-        if (self.frame == undefined or
-            self.resume_at == 0 or
-            self.resume_at > ticks)
-        {
-            return;
-        }
-
-        var fr = self.frame;
-        self.resume_at = 0;
-        self.frame = undefined;
-        resume fr;
-    }
-
-    pub fn sleep(self: *Self, ms: u32) void {
-        suspend {
-            self.resume_at = ticks + ms;
-            self.frame = @frame();
-        }
-    }
-};
-
-var tim_counter: u16 = 0;
-var tim_overflows: u16 = 0;
 
 pub fn main() void {
     chip.init();
     chip.systick(1);
-    chip.led.off();
 
+    pwm_init();
+    while (true) {}
+}
+
+fn pwm_init() void {
     // enable output pin for the timer
     // PB8, pin B port 8 to alternate function T10_CH1
     regs.RCC.AHB1ENR.modify(.{ .GPIOBEN = 1 }); // enable gpio port B
@@ -90,74 +38,4 @@ pub fn main() void {
 
     // enable the counter
     regs.TIM10.CR1.modify(.{ .CEN = 1 });
-
-    // to enable timer interrupt:
-    regs.TIM10.DIER.modify(.{ .UIE = 1 }); // update interrupt enable
-    regs.NVIC.ISER0.modify(.{ .SETENA = 1 << 25 }); // enable IRQn 25 in cpu nvic
-
-    nosuspend start_tasks();
-
-    while (true) {
-        // Read current counter value
-        tim_counter = regs.TIM10.CNT.read();
-        asm volatile ("nop");
-    }
-}
-
-var tasks: [2]*Task = undefined;
-
-fn start_tasks() void {
-    var t1 = Task{};
-    tasks[0] = &t1;
-    var fr1 = async blink_loop(&t1, 1000);
-
-    var t2 = Task{};
-    tasks[1] = &t2;
-    var fr2 = async uart_loop(&t2);
-
-    await fr1;
-    await fr2;
-}
-
-fn tick() void {
-    ticks += 1;
-    for (tasks) |task| {
-        if (task == undefined) {
-            continue;
-        }
-        task.try_run();
-    }
-}
-
-fn blink_loop(task: *Task, sleep: u32) void {
-    chip.led.off();
-    while (true) {
-        task.sleep(sleep);
-        chip.led.on();
-        task.sleep(50);
-        chip.led.off();
-    }
-}
-
-fn uart_loop(task: *Task) void {
-    const uart_idx = 2;
-    const pins = .{ .tx = micro.Pin("PA2"), .rx = micro.Pin("PA3") };
-
-    var uart = micro.Uart(uart_idx, pins).init(.{
-        .baud_rate = 9600,
-        .stop_bits = .one,
-        .parity = null,
-        .data_bits = .eight,
-    }) catch {
-        return;
-    };
-
-    var out = uart.writer();
-
-    var buf: [128]u8 = undefined;
-    while (true) {
-        const s = std.fmt.bufPrint(buf[0..], "tim9 counter: {d} overflows: {d} \r\n", .{ tim_counter, tim_overflows }) catch buf[0..0];
-        try out.writeAll(s);
-        task.sleep(1000);
-    }
 }
